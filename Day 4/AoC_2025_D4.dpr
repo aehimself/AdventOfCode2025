d772555dddf1d8456d7ad8dba05f10b000409026
AEHimself <57598335+aehimself@users.noreply.github.com>
2025-12-09 23:07:57 +0100

    Day 4 challenge

---
+++ Day 4/AoC_2025_D4.dpr
@@ -0,0 +1,73 @@
+﻿Program AoC_2025_D4;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils, System.IOUtils,
+  uFloorPlan in 'uFloorPlan.pas';
+
+Var
+  floorplan: TFloorPlan;
+  lines: TArray<String>;
+  row, col: NativeInt;
+  count, totalremovable: Integer;
+
+Begin
+  totalremovable := 0;
+
+  Try
+    Try
+      lines := TFile.ReadAllLines('.\floorplan.txt');
+
+      floorplan := TFloorPlan.Create;
+      Try
+        floorplan.SetMapDimensions(Length(lines[0]), Length(lines));
+
+        WriteLn('*** Filling floor plan...');
+        For row := Low(lines) To High(lines) Do
+        Begin
+          WriteLn(lines[row]);
+
+          For col := 0 To Length(lines[row]) - 1 Do
+            If lines[row].Chars[col] = '@' Then
+              floorplan[col, row] := moPaperRoll
+            Else
+              floorplan[col, row] := moEmpty;
+        End;
+
+        WriteLn;
+
+        WriteLn('*** Counting accessible paper rolls...');
+
+        For row := 0 To floorplan.MapHeight - 1 Do
+          For col := 0 To floorplan.MapWidth - 1 Do
+          Begin
+            If floorplan[col, row] <> moPaperRoll Then
+              Continue;
+
+            count := floorplan.NeighbouringPaperRolls(col, row);
+
+            If count <= 3 Then
+            Begin
+              Inc(totalremovable);
+
+              WriteLn('Paper roll at row ' + row.ToString + ' column ' + col.ToString + ' has only ' + count.ToString + ' neighbouring paper rolls!');
+            End;
+          End;
+
+        WriteLn('A total of ' + totalremovable.ToString + ' paper rolls can be accessed by a forklift.');
+
+        WriteLn;
+      Finally
+        FreeAndNil(floorplan);
+      End;
+    Except
+      On E: Exception Do
+        Writeln(E.ClassName, ': ', E.Message);
+    End;
+  Finally
+    ReadLn;
+  End;
+End.
---
+++ Day 4/uFloorPlan.pas
@@ -0,0 +1,103 @@
+﻿Unit uFloorPlan;
+
+Interface
+
+Uses System.SysUtils;
+
+Type
+  TMapObject = ( moEmpty, moPaperRoll );
+
+  TFloorPlan = Class
+  strict private
+    _map: TArray<TArray<TMapObject>>;
+    Procedure SetMapObject(Const inColumn, inRow: NativeInt; Const inMapObject: TMapObject);
+    Function GetMapHeight: NativeInt;
+    Function GetMapObject(Const inColumn, inRow: NativeInt): TMapObject;
+    Function GetMapWidth: NativeInt;
+  public
+    Constructor Create; ReIntroduce;
+    Procedure ClearMap;
+    Procedure SetMapDimensions(Const inColumnAmount, inRowAmount: NativeInt);
+    Function NeighbouringPaperRolls(Const inColumn, inRow: NativeInt): Integer;
+    Property MapHeight: NativeInt Read GetMapHeight;
+    Property MapWidth: NativeInt Read GetMapWidth;
+    Property MapObject[Const inColumn, inRow: NativeInt]: TMapObject Read GetMapObject Write SetMapObject; Default;
+  End;
+
+  EFloorPlanException = Class(Exception);
+
+Implementation
+
+//
+// TFloorPlan
+//
+
+Procedure TFloorPlan.ClearMap;
+Var
+  a: NativeInt;
+Begin
+  For a := Low(_map) To High(_map) Do
+    FillChar(_map[a][0], Length(_map[a]), 0);
+End;
+
+Constructor TFloorPlan.Create;
+Begin
+  inherited;
+
+  SetMapDimensions(10, 10);
+  ClearMap;
+End;
+
+Function TFloorPlan.GetMapHeight: NativeInt;
+Begin
+  Result := Length(_map);
+End;
+
+Function TFloorPlan.GetMapObject(Const inColumn, inRow: NativeInt): TMapObject;
+Begin
+  Result := _map[inRow][inColumn];
+End;
+
+Function TFloorPlan.GetMapWidth: NativeInt;
+Begin
+  If Length(_map) = 0 Then
+    Result := 0
+  Else
+    Result := Length(_map[0]);
+End;
+
+Function TFloorPlan.NeighbouringPaperRolls(Const inColumn, inRow: NativeInt): Integer;
+Var
+  row, col: NativeInt;
+Begin
+  If (inColumn < 0) Or (inColumn >= Self.MapWidth) Or (inRow < 0) Or (inRow >= Self.MapHeight) Then
+    Raise EFloorPlanException.Create('Invalid floor plan position!');
+
+  Result := 0;
+
+  For col := -1 To 1 Do
+    For row := -1 To 1 Do
+      If (Abs(row) + Abs(col) <> 0) And (inColumn + col >= 0) And (inColumn + col < Self.MapWidth) And (inRow + row >= 0) And
+         (inRow + row < Self.MapWidth) And (_map[inRow + row][inColumn + col] = moPaperRoll) Then
+        Inc(Result);
+End;
+
+Procedure TFloorPlan.SetMapDimensions(Const inColumnAmount, inRowAmount: NativeInt);
+Var
+  a: NativeInt;
+Begin
+  If (inColumnAmount = 0) Or (inRowAmount = 0) Then
+    Raise EFloorPlanException.Create('Invalid floor plan dimansions!');
+
+  SetLength(_map, inRowAmount);
+
+  For a := Low(_map) To High(_map) Do
+    SetLength(_map[a], inColumnAmount);
+End;
+
+Procedure TFloorPlan.SetMapObject(Const inColumn, inRow: NativeInt; Const inMapObject: TMapObject);
+Begin
+  _map[inRow][inColumn] := inMapObject;
+End;
+
+End.
