5edbefe6dd0a06bd8658b5908adc6f95cd8f7b09
AEHimself <57598335+aehimself@users.noreply.github.com>
2025-12-09 19:36:15 +0100

    Day 3 challenge

---
+++ Day 3/AoC_2025_D3.dpr
@@ -0,0 +1,45 @@
+﻿Program AoC_2025_D3;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils, System.IOUtils,
+  uBatteryBank in 'uBatteryBank.pas';
+
+Var
+  line: String;
+  banks: TBatteryBanks;
+  bank: TBatteryBank;
+  totaljolts: Integer;
+
+Begin
+  totaljolts := 0;
+
+  Try
+    Try
+      For line In TFile.ReadAllLines('.\batterybanks.txt') Do
+      Begin
+        banks := TBatteryBanks.Create;
+        Try
+          bank := banks.AddBank(line);
+
+          WriteLn('Battery bank: ' + line + ', maximum joltage: ' + bank.MaxJoltage.ToString);
+
+          totaljolts := totaljolts + bank.MaxJoltage;
+        Finally
+          FreeAndNil(banks);
+        End;
+      End;
+
+      WriteLn;
+      WriteLn('Total battery joltage: ' + totaljolts.ToString);
+    Except
+      On E: Exception Do
+        Writeln(E.ClassName, ': ', E.Message);
+    End;
+  Finally
+    ReadLn;
+  End;
+End.
---
+++ Day 3/uBatteryBank.pas
@@ -0,0 +1,84 @@
+﻿Unit uBatteryBank;
+
+Interface
+
+Uses System.Generics.Collections;
+
+Type
+  TBatteryBank = Class
+  strict private
+    _batteries: TArray<Byte>;
+    Function LargestNumberFromPos(Const inStartPos: NativeInt; Const inIgnoreLast: Boolean): NativeInt;
+  public
+    Constructor Create(Const inBankString: String); ReIntroduce;
+    Function MaxJoltage: Byte;
+  End;
+
+  TBatteryBanks = Class(TObjectList<TBatteryBank>)
+  public
+    Function AddBank(Const inBankString: String): TBatteryBank;
+  End;
+
+Implementation
+
+Uses System.SysUtils;
+
+//
+// TBatteryBank
+//
+
+Constructor TBatteryBank.Create(Const inBankString: String);
+Var
+  a: NativeInt;
+Begin
+  inherited Create;
+
+  SetLength(_batteries, inBankString.Length);
+
+  For a := Low(_batteries) To High(_batteries) Do
+    _batteries[a] := Byte.Parse(inBankString.Chars[a]);
+End;
+
+Function TBatteryBank.LargestNumberFromPos(Const inStartPos: NativeInt; Const inIgnoreLast: Boolean): NativeInt;
+Var
+  a, last: NativeInt;
+Begin
+  Result := inStartPos;
+
+  last := High(_batteries);
+
+  If inIgnoreLast Then
+    last := last - 1;
+
+  For a := inStartPos + 1 To last Do
+  Begin
+    If _batteries[a] > _batteries[Result] Then
+      Result := a;
+
+    If _batteries[Result] = 9 Then
+      Break;
+  End;
+End;
+
+Function TBatteryBank.MaxJoltage: Byte;
+Var
+  first, second: NativeInt;
+Begin
+  first := LargestNumberFromPos(0, True);
+  second := LargestNumberFromPos(first + 1, False);
+
+  Result := Byte.Parse(_batteries[first].ToString + _batteries[second].ToString);
+End;
+
+//
+// TBatteryBanks
+//
+
+Function TBatteryBanks.AddBank(Const inBankString: String): TBatteryBank;
+Begin
+  Result := TBatteryBank.Create(inBankString);
+
+  Self.Add(Result);
+End;
+
+End.
