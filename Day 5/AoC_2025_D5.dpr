b4e650ca88aca904c800f318c19d80c8d93ac785
AEHimself <57598335+aehimself@users.noreply.github.com>
2025-12-09 23:50:00 +0100

    Day 5 challenge

---
+++ Day 5/AoC_2025_D5.dpr
@@ -0,0 +1,72 @@
+﻿Program AoC_2025_D5;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils, System.IOUtils,
+  uInventoryManager in 'uInventoryManager.pas';
+
+Var
+  invmgr: TInventoryManager;
+  line: String;
+  available: Boolean;
+  ingredient: TIngredientID;
+  fresh: Integer;
+
+Begin
+  available := False;
+  fresh := 0;
+
+  Try
+    Try
+      invmgr := TInventoryManager.Create;
+      Try
+        WriteLn('Parsing fresh ingredient ranges...');
+
+        For line In TFile.ReadAllLines('.\ingredients.txt') Do
+        Begin
+          If line.IsEmpty Then
+          Begin
+            WriteLn;
+            WriteLn('End of fresh ingredients. Parsing available ingredients...');
+
+            available := True;
+
+            Continue;
+          End;
+
+          If available Then
+            invmgr.AddAvailable(TIngredientID.Parse(line))
+          Else
+            invmgr.AddFresh(line);
+
+          WriteLn(line);
+        End;
+
+        WriteLn;
+        WriteLn('Checking for fresh ingredients...');
+
+        For ingredient In invmgr.AvailableIngredients Do
+          If invmgr.IsFresh(ingredient) Then
+          Begin
+            WriteLn('Ingredient ' + ingredient.ToString + ' is fresh!');
+
+            Inc(fresh);
+          End;
+
+        WriteLn;
+        WriteLn('A total of ' + fresh.ToString + ' ingredient(s) are fresh.');
+
+      Finally
+        FreeAndNil(invmgr);
+      End;
+    Except
+      On E: Exception Do
+        Writeln(E.ClassName, ': ', E.Message);
+    End;
+  Finally
+    ReadLn;
+  End;
+End.
---
+++ Day 5/uInventoryManager.pas
@@ -0,0 +1,91 @@
+﻿Unit uInventoryManager;
+
+Interface
+
+Uses System.Generics.Collections, System.SysUtils;
+
+Type
+  TIngredientID = UInt64;
+
+  TIngredientRange = TPair<TIngredientID, TIngredientID>;
+
+  TInventoryManager = Class
+  strict private
+    _available: TList<TIngredientID>;
+    _fresh: TList<TIngredientRange>;
+    Function GetAvailableIngredients: TArray<TIngredientID>;
+  public
+    Constructor Create; ReIntroduce;
+    Destructor Destroy; Override;
+    Procedure AddAvailable(Const inAvailable: TIngredientID);
+    Procedure AddFresh(Const inFreshString: String);
+    Function IsFresh(Const inIngredient: TIngredientID): Boolean;
+    Property AvailableIngredients: TArray<TIngredientID> Read GetAvailableIngredients;
+  End;
+
+  EInventoryManagerException = Class(Exception);
+
+Implementation
+
+//
+// TInventoryManager
+//
+
+Procedure TInventoryManager.AddAvailable(Const inAvailable: TIngredientID);
+Begin
+  _available.Add(inAvailable);
+End;
+
+Procedure TInventoryManager.AddFresh(Const inFreshString: String);
+Var
+  freshstart, freshend: TIngredientID;
+Begin
+  If inFreshString.CountChar('-') <> 1 Then
+    Raise EInventoryManagerException.Create('Invalid fresh ingredient line!');
+
+  freshstart := TIngredientID.Parse(inFreshString.Substring(0, inFreshString.IndexOf('-')));
+  freshend := TIngredientID.Parse(inFreshString.Substring(inFreshString.IndexOf('-') + 1));
+
+  If freshstart > freshend Then
+    Raise EInventoryManagerException.Create('Invalid fresh ingredient range!');
+
+  _fresh.Add(TIngredientRange.Create(freshstart, freshend));
+End;
+
+Constructor TInventoryManager.Create;
+Begin
+  inherited;
+
+  _available := TList<TIngredientID>.Create;
+  _fresh := TList<TIngredientRange>.Create;
+End;
+
+Destructor TInventoryManager.Destroy;
+Begin
+  FreeAndNil(_available);
+  FreeAndNil(_fresh);
+
+  inherited;
+End;
+
+Function TInventoryManager.GetAvailableIngredients: TArray<TIngredientID>;
+Begin
+  Result := _available.ToArray;
+End;
+
+Function TInventoryManager.IsFresh(Const inIngredient: TIngredientID): Boolean;
+Var
+  range: TIngredientRange;
+Begin
+  Result := False;
+
+  For range In _fresh Do
+  Begin
+    Result := (inIngredient >= range.Key) And (inIngredient <= range.Value);
+
+    If Result Then
+      Break;
+  End;
+End;
+
+End.
